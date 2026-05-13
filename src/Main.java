f0c1dcb3338d6e179274880b341e4199c1dac9f5
David Tuaillon <david.tuaillon68@gmail.com>
2026-05-13 22:58:19 +0200

    ajout de la classe game avec package engine

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import inscryption.engine.Game;
 
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("Another challenger... It has been ages.");
+
+        Game jeu = new Game();
+        jeu.lancerJeu();
+
     }
 }
---
+++ src/inscryption/engine/Game.java
@@ -0,0 +1,15 @@
+package inscryption.engine;
+
+public final class Game
+{
+    private final int NB_DE_PARTIES_POUR_GAGNER = 3;
+    private final int NB_DE_POINTS_POUR_GAGNER_PARTIE = 5;
+
+    
+
+
+    public Game() {};
+
+    public void lancerJeu() {};
+
+}
